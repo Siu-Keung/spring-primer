f3d75b10d6e79027537ac2c749177c17e445dbc5
SiuKeung <siu_keung@outlook.com>
2018-07-22 13:03:55 +0800

    编写了启动类

---
+++ src/main/java/Application.java
@@ -0,0 +1,14 @@
+/**
+ * @author Dylan Wei
+ * @date 2018-07-22 13:02
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
