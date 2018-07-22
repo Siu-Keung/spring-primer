140f10d0bd50846609df7c186ccd382488b34f40
SiuKeung <siu_keung@outlook.com>
2018-07-22 14:14:42 +0800

    配置了名叫datas的bean，该bean保存了todo list的全部数据

--- src/main/java/com/oocl/springboot/Application.java
+++ src/main/java/com/oocl/springboot/Application.java
@@ -3,9 +3,15 @@ package com.oocl.springboot; /**
  * @date 2018-07-22 13:02
  */
 import com.oocl.springboot.controller.GreetingController;
+import com.oocl.springboot.model.Filter;
+import com.oocl.springboot.model.Item;
+import com.oocl.springboot.model.TodoDataModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -14,6 +20,20 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public TodoDataModel datas(){
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("3123132", "做作业", false));
+        items.add(new Item("3423424", "看电视", false));
+        items.add(new Item("12342423", "玩游戏", true));
+        items.add(new Item("13424242", "运动", false));
+        List<Filter> allFilters = new ArrayList<>();
+        allFilters.add(new Filter("全部", true));
+        allFilters.add(new Filter("未完成", true));
+        allFilters.add(new Filter("已完成", true));
+        TodoDataModel datas = new TodoDataModel(items, allFilters);
+        return datas;
+    }
 
 
 }
